87a6bf0680b05c23c652d0779f5617f136207470
Lemmawool <treyottaway@gmail.com>
2018-01-02 17:51:39 -0500

    Housing Prices First Attempt

---
+++ Housing Prices/featureNormGradDesc.R
@@ -0,0 +1,44 @@
+# The following code uses gradient decent with feature normalization 
+# to find a solution for the House Prices:Advanced Regression Techniques
+# Kaggle Competition.
+
+# Load functions and data sets
+source("featureNormalize.R")
+source("gradientDescent.R")
+source("normalEquation.R")
+train <- read.csv("~/Kaggle Projects/Housing Prices/train.csv", header=FALSE)
+test <- read.csv("~/Kaggle Projects/Housing Prices/test.csv", header=FALSE)
+
+# Convert the rows and columns in train and test to numeric values.
+train_matrix <- sapply(train, as.numeric)
+train_matrix[is.na(train)] <- 0
+test_matrix <- sapply(test, as.numeric)
+test_matrix[is.na(test)] <- 0
+
+# Compute theta using a normalized set of features.
+num_features <- length(train_matrix[1,])
+X_train <- train_matrix[,1:(num_features - 1)]
+y_train <- train_matrix[,num_features]
+m_train <- length(y_train)
+
+X_train_norm <- featureNormalize(X_train)
+X_train_norm <- cbind(rep(1,m_train), X_train_norm)
+
+X_test <- test_matrix[,1:(num_features - 1)]
+m_test <- length(X_test[,1])
+X_test_norm <- featureNormalize(X_test)
+X_test_norm <- cbind(rep(1,m_test), X_test_norm)
+
+theta <- c(rep(0,num_features))
+theta <- gradientDescent(X_train_norm, y_train, theta, 0.005, 400)
+
+# Compute the SalePrice for the test data and create our submission.
+test_costs <- X_test_norm %*% theta
+test_costs <- abs(tail(test_costs, length(test_costs) - 1))*1000
+test_ids <- tail(test$V1, length(test$V1) - 1)
+submission <- data.frame(Ids = test_ids, SalePrice = test_costs)
+write.csv(submission, file = "featureNormGradDesc.csv", quote = FALSE, row.names = FALSE)
+# Score of 0.64944
+
+
+
